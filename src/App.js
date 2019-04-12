867496be0d5339de6c67ea87cbc007dfdf77478f
tricia albert <tricia.albert@gmail.com>
2019-04-12 11:53:28 -0400

    List Chat Rooms commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+ var config = {
+    apiKey: "AIzaSyDHrUXRzGSi-ZVdZO578ZhnD6ELOcdxrrU",
+    authDomain: "bloc-chat-react-c3719.firebaseapp.com",
+    databaseURL: "https://bloc-chat-react-c3719.firebaseio.com",
+    projectId: "bloc-chat-react-c3719",
+    storageBucket: "bloc-chat-react-c3719.appspot.com",
+    messagingSenderId: "484693895730"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <RoomList database={firebase}/>
       </div>
     );
   }
