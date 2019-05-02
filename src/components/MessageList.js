f6d21c4204b8ba6940e7935bffe1a7589e6cfbb5
tricia albert <tricia.albert@gmail.com>
2019-05-01 22:12:58 -0400

    FINALLY FIXED IT.  FEELIN' LIKE A NINJA!

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-class MessageList extends Component {
-  constructor(props) {
-  	super(props);
-  	this.state = {
-  		messages: [],
-  		username: '',
-  		content: '',
-  		sentAt: '',
-  		roomId: ''
-  	}
-  	this.messagesRef = this.props.database.database().ref('Messages');
-  }	
- componentDidMount(){
-        this.messagesRef.on('child_added', snapshot => {
-            const messages = {value : snapshot.val(),
-            key : snapshot.key};
-           this.setState({messages: this.state.messages.concat(messages)})
-        })
+class MessageList extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            messages: [],
+            username: '',
+            content: '',
+            sentAt: '',
+            roomId: ''
+        }
+        this.messagesRef = this.props.database.database().ref('Messages')
     }
-  render(){
-    return(
+    componentDidMount(){
+        this.messagesRef.on('child_added', snapshot => {
+         const message = {
+             value : snapshot.val(),
+             key : snapshot.key};  
+         this.setState({messages : this.state.messages.concat(message)})
+        })
+    }   
+   
+    render(){
+        return(
         <div className='message-list'>
            {this.state.messages
            .filter(message => this.props.activeRoom.key === message.value.roomId)
